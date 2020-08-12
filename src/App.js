1fc6e675eb8c22607b814129e91e7745e9bc32b6
TwentyFive <TwentyFiveSoftware@gmail.com>
2020-08-12 18:20:01 +0200

    Added general style for project section.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faAngleDown} from '@fortawesome/free-solid-svg-icons';
-// import {faGithub, faItchIo} from '@fortawesome/free-brands-svg-icons';
+import {faAngleDown, faAngleRight, faCalculator} from '@fortawesome/free-solid-svg-icons';
+import {faGithub} from '@fortawesome/free-brands-svg-icons';
 // import {faEnvelope} from '@fortawesome/free-regular-svg-icons';
 import TwentyFiveIcon from './assets/icon.png';
 
@@ -15,19 +15,19 @@ export default function App() {
                 <a href={'#projects'} className={'landing-zone__angle'}><FontAwesomeIcon icon={faAngleDown}/></a>
             </div>
             <div className={'projects-zone'} id={'projects'}>
-                <div className={'projects-zone__title-container'}>
+                <div>
                     <div className={'projects-zone__title'}>PROJECTS</div>
                     <div className={'projects-zone__subtitle'}>A selection of my latest projects that you can find on Github.</div>
                 </div>
                 <div className={'projects-zone__list'}>
                     <div className={'project'}>
-                        <div className={'project__image'}>LOGO</div>
+                        <div className={'project__image'}><FontAwesomeIcon icon={faCalculator}/></div>
                         <div className={'project__info'}>
                             <div className={'project__name'}>Calculator</div>
                             <div className={'project__description'}>A simple minimalistic calculator for the web.</div>
                             <div className={'project__buttons'}>
-                                <a href={'https://twentyfivesoftware.github.io/calculator/'} className={'button'}>Open Website</a>
-                                <a href={'https://www.github.com/TwentyFiveSoftware/calculator'} className={'button'}>View on Github</a>
+                                <a href={'https://twentyfivesoftware.github.io/calculator/'} className={'button button--icon-right'}>Open Website<FontAwesomeIcon icon={faAngleRight}/></a>
+                                <a href={'https://www.github.com/TwentyFiveSoftware/calculator'} className={'button'}><FontAwesomeIcon icon={faGithub}/>Visit on Github</a>
                             </div>
                         </div>
                     </div>
