ea6f70756d5048e79ad13229fd6fb3a5944a5937
TwentyFive <TwentyFiveSoftware@gmail.com>
2020-08-12 20:02:12 +0200

    Added hover effect to social buttons.

--- src/App.js
+++ src/App.js
@@ -78,9 +78,9 @@ export default function App() {
                 </div>
             </div>
             <div className={'footer'}>
-                <a href={'https://www.github.com/TwentyFiveSoftware'}><FontAwesomeIcon icon={faGithub}/></a>
-                <a href={'mailto:TwentyFiveSoftware@gmail.com'}><FontAwesomeIcon icon={faEnvelope}/></a>
-                <a href={'https://twentyfive.itch.io/'}><FontAwesomeIcon icon={faItchIo}/></a>
+                <a className={'social-button'} href={'https://www.github.com/TwentyFiveSoftware'}><FontAwesomeIcon icon={faGithub}/></a>
+                <a className={'social-button'} href={'mailto:TwentyFiveSoftware@gmail.com'}><FontAwesomeIcon icon={faEnvelope}/></a>
+                <a className={'social-button'} href={'https://twentyfive.itch.io/'}><FontAwesomeIcon icon={faItchIo}/></a>
             </div>
         </div>
     );
