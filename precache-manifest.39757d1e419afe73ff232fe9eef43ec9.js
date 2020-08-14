3bc26c23272156bc43a38f3c66d387ec39a0fdb8
TwentyFive <TwentyFiveSoftware@gmail.com>
2020-08-14 15:40:20 +0200

    Updates

---
+++ precache-manifest.39757d1e419afe73ff232fe9eef43ec9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f2fdc5feb355548865c0801ab60b294",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7189752fdeec6f63157d",
+    "url": "/static/css/main.a9dd8969.chunk.css"
+  },
+  {
+    "revision": "6c7c9849dde9a22b66c4",
+    "url": "/static/js/2.05cda39c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.05cda39c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7189752fdeec6f63157d",
+    "url": "/static/js/main.adffa215.chunk.js"
+  },
+  {
+    "revision": "d2b199ecc53dfac6fd15",
+    "url": "/static/js/runtime-main.0993b479.js"
+  },
+  {
+    "revision": "797ae5bb8882857f6ac7ebe161bdebdd",
+    "url": "/static/media/icon.797ae5bb.svg"
+  },
+  {
+    "revision": "df238c34e2ba6cd2378e217fba012fa5",
+    "url": "/static/media/mountain.df238c34.webp"
+  },
+  {
+    "revision": "1934791ad04dcdd930139a6d2ebeecde",
+    "url": "/static/media/twentyfivesoftware-logo.1934791a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1919fab6fb926479659d8cb79f376aa3.js"
+  "/precache-manifest.39757d1e419afe73ff232fe9eef43ec9.js"
 );
 
 self.addEventListener('message', (event) => {
