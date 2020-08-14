169a89020e50b1d27ecba8ec494f1219f75f389b
TwentyFive <TwentyFiveSoftware@gmail.com>
2020-08-14 13:48:37 +0200

    Added fade animation to projects.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade';
+
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faAngleDown, faAngleRight, faCalculator, faNewspaper} from '@fortawesome/free-solid-svg-icons';
 import {faGithub, faItchIo} from '@fortawesome/free-brands-svg-icons';
 import {faEnvelope} from '@fortawesome/free-regular-svg-icons';
+
 import TwentyFiveIcon from './assets/icon.png';
 import FuncturyLogo from './assets/functury-logo.png';
 
@@ -68,19 +71,21 @@ export default function App() {
                 </div>
                 <div className={'projects-zone__list'}>
                     {projects.map((project, index) =>
-                        <div className={'project'} key={index}>
-                            <div className={'project__image'} style={{background: `linear-gradient(to bottom right, ${project.colors.from}, ${project.colors.to})`}}>
-                                {project.image}
-                            </div>
-                            <div className={'project__info'}>
-                                <div className={'project__name'}>{project.name}</div>
-                                <div className={'project__description'}>{project.description}</div>
-                                <div className={'project__buttons'}>
-                                    <a href={project.projectUrl} className={'button button--icon-right'} style={{color: project.colors.buttons, borderColor: project.colors.buttons}}>Open Website<FontAwesomeIcon icon={faAngleRight}/></a>
-                                    <a href={project.githubUrl} className={'button'} style={{color: project.colors.buttons, borderColor: project.colors.buttons}}><FontAwesomeIcon icon={faGithub}/>Visit on Github</a>
+                        <Fade bottom distance={'5em'}>
+                            <div className={'project'} key={index}>
+                                <div className={'project__image'} style={{background: `linear-gradient(to bottom right, ${project.colors.from}, ${project.colors.to})`}}>
+                                    {project.image}
+                                </div>
+                                <div className={'project__info'}>
+                                    <div className={'project__name'}>{project.name}</div>
+                                    <div className={'project__description'}>{project.description}</div>
+                                    <div className={'project__buttons'}>
+                                        <a href={project.projectUrl} className={'button button--icon-right'} style={{color: project.colors.buttons, borderColor: project.colors.buttons}}>Open Website<FontAwesomeIcon icon={faAngleRight}/></a>
+                                        <a href={project.githubUrl} className={'button'} style={{color: project.colors.buttons, borderColor: project.colors.buttons}}><FontAwesomeIcon icon={faGithub}/>Visit on Github</a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        </Fade>
                     )}
                 </div>
             </div>
