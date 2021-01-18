34d066c15da8ef98cca2b6aae861082a03443f0c
TwentyFive <TwentyFiveSoftware@gmail.com>
2021-01-18 22:33:46 +0100

    add fileshare project

--- src/content.tsx
+++ src/content.tsx
@@ -1,4 +1,4 @@
-import {faCalculator, faNewspaper, faVideo} from '@fortawesome/free-solid-svg-icons';
+import {faCalculator, faNewspaper, faShare, faVideo} from '@fortawesome/free-solid-svg-icons';
 import {faYoutube} from '@fortawesome/free-brands-svg-icons';
 
 import FuncturyLogo from './assets/functury-logo.png';
@@ -10,10 +10,16 @@ export const COLORS: Color[] = [
     {from: '#FCB76B', to: '#FB8080', buttons: '#FC9C76'},
     {from: '#8EF7AA', to: '#DFF494', buttons: '#B7F69F'},
     {from: '#9CFCF8', to: '#6E7BFB', buttons: '#83B8F9'},
+    {from: '#7BCAD1', to: '#77A1D3', buttons: '#7BCAD1'},
     {from: '#D7B6FC', to: '#8460FC', buttons: '#B795FC'}
 ];
 
 export const PROJECTS: Project[] = [
+    {
+        name: 'FileShare',
+        githubRepoName: 'fileshare',
+        image: {fontAwesomeIcon: faShare}
+    },
     {
         name: 'Watch2Gether (Clone)',
         githubRepoName: 'watch2gether-client',
